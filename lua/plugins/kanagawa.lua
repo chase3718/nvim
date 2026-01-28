af7b6e4c1eec5a2252f6779dad1df73a5dcf3a31
Chase Williams <chasewilliams98@gmail.com>
2026-01-28 01:08:48 -0500

    init commit

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,14 @@
+return {
+  -- add kanagawa
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load the colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
+
+
